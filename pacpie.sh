71210b2766575d9c96c0cc4f516e1cebac4564e8
MohamedAHna <mohamed.ahmed.altaee123@gmail.com>
2022-07-22 06:54:16 +0300

    adding pacpie.sh as the first file.

---
+++ pacpie.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+alias pieIns='sudo pacman -S'
+alias pieSea='pacman -Ss'
+alias pieSeaf='pacman -Qs'
+alias pieRem='sudo pacman -R'
+alias pieRems='sudo pacman -Rs'
+alias pieupdg='sudo pacman -Syyu'
